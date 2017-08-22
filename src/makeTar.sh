563a6ed82548a1ca89828ca692ce4dbadb72c3e4
awhitbeck <whitbeck.andrew@gmail.com>
2017-08-22 15:21:25 -0500

    rename Higgs to Z

--- src/makeTar.sh
+++ src/makeTar.sh
@@ -2,4 +2,4 @@
 
 cd $CMSSW_BASE/../
 tar -cf workingArea.tar CMSSW_7_4_2 --exclude='*.dag.*' --exclude='*tar' --exclude='CMSSW_7_4_2/*/src/*root' --exclude='*eps' --exclude='*png' --exclude='*pdf' --exclude='*stdout' --exclude='*stderr' --exclude='*condor' --exclude='CMSSW_7_4_2/*/bin/*'
-mv workingArea.tar CMSSW_7_4_2/src/boostedHiggsPlusMET/src/.
+mv workingArea.tar CMSSW_7_4_2/src/boostedZPlusMET/src/.
