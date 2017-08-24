6587c0c079eba32c7e59d28a89aad96c2205228f
awhitbeck <whitbeck.andrew@gmail.com>
2017-08-24 10:20:11 -0500

    adding hadd commands for singleLep stuff

--- src/runScaleFactors.sh
+++ src/runScaleFactors.sh
@@ -10,29 +10,32 @@ COMMIT=$1
 
 echo "COMMIT: ${COMMIT}"
 
+hadd -f ~/eos/boostedZPlusMET/$COMMIT/ALPHABEThistos_singleLep.root ~/eos/boostedZPlusMET/$COMMIT/ALPHABEThistos_singleEle.root ~/eos/boostedZPlusMET/$COMMIT/ALPHABEThistos_singleMu.root
+hadd -f ~/eos/boostedZPlusMET/$COMMIT/ALPHABEThistos_looseCuts_singleLep.root ~/eos/boostedZPlusMET/$COMMIT/ALPHABEThistos_looseCuts_singleEle.root ~/eos/boostedZPlusMET/$COMMIT/ALPHABEThistos_looseCuts_singleMu.root
+
 regions="_looseCuts_photon _photon _singleMu _looseCuts_singleMu _singleEle _looseCuts_singleEle _singleLep _looseCuts_singleLep _lowDphi _looseCuts_lowDphi"
 
 echo "^^^^ Signal region ^^^^^" 
 echo ">>>> >100 "
 root -l -n -b -q 'checkScaleFactors.cc("",true,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
-#root -l -n -b -q 'checkScaleFactors.cc("",false,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
+root -l -n -b -q 'checkScaleFactors.cc("",false,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
 echo ">>>> >100 fine"
 root -l -n -b -q 'checkScaleFactors.cc("",true,"~/eos/boostedZPlusMET/'$COMMIT'/","METfine",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
-#root -l -n -b -q 'checkScaleFactors.cc("",false,"~/eos/boostedZPlusMET/'$COMMIT'/","METfine",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
+root -l -n -b -q 'checkScaleFactors.cc("",false,"~/eos/boostedZPlusMET/'$COMMIT'/","METfine",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
 echo ">>>> >300 "
 root -l -n -b -q 'checkScaleFactors.cc("",true,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",2,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
-#root -l -n -b -q 'checkScaleFactors.cc("",false,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",2,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
+root -l -n -b -q 'checkScaleFactors.cc("",false,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",2,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
 
 for region in $regions 
 do
     echo "^^^^ "$region" region ^^^^^" 
     echo ">>>> >100 "
     root -l -n -b -q 'checkScaleFactors.cc("'$region'",true,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
-    #root -l -n -b -q 'checkScaleFactors.cc("'$region'",false,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
+    root -l -n -b -q 'checkScaleFactors.cc("'$region'",false,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
     echo ">>>> >100 fine"
     root -l -n -b -q 'checkScaleFactors.cc("'$region'",true,"~/eos/boostedZPlusMET/'$COMMIT'/","METfine",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
-    #root -l -n -b -q 'checkScaleFactors.cc("'$region'",false,"~/eos/boostedZPlusMET/'$COMMIT'/","METfine",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
+    root -l -n -b -q 'checkScaleFactors.cc("'$region'",false,"~/eos/boostedZPlusMET/'$COMMIT'/","METfine",1,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
     echo ">>>> >300 "
     root -l -n -b -q 'checkScaleFactors.cc("'$region'",true,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",2,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
-    #root -l -n -b -q 'checkScaleFactors.cc("'$region'",false,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",2,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
+    root -l -n -b -q 'checkScaleFactors.cc("'$region'",false,"~/eos/boostedZPlusMET/'$COMMIT'/","MET",2,4,"~/eos/boostedZPlusMET/'$COMMIT'/plots/ABCDscaleFactors/")'
 done
