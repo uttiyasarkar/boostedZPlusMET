82e80dc5f1f6f657993705fed7804cefedf5e30c
awhitbeck <whitbeck.andrew@gmail.com>
2017-08-30 14:08:49 -0500

    an attempt, but it doesn't seem to work.  root file transfer is broken and I am not even sure I know where the log files are stored, maybe on the head node?

--- src/worker.sh
+++ src/worker.sh
@@ -5,6 +5,8 @@ args1=$3
 args2=$4
 args3=$5
 args4=$6
+args5=$7
+args6=$8
 echo "output_dir ${output_dir}"
 echo "executable ${executable}"
 echo "args ${args}"
@@ -21,10 +23,13 @@ echo "PATH ${PATH}"
 cd src
 make all
 
-`echo $executable $args1 $args2 $args3 $args4`
+`echo $executable $args1 $args2 $args3 $args4 $args5 $args6`
 
 ls *root
 ls ../plots/*/*
+ls $CMSSW_BASE/../*condor
+ls $CMSSW_BASE/../*stdout
+ls $CMSSW_BASE/../*stderr
 
 for file in $(ls *root)
 do
@@ -36,3 +41,24 @@ for file in $(ls plots/*/*)
 do 
     xrdcp -f $file root://cmseos.fnal.gov//store/user/${USER}/boostedZPlusMET/$output_dir/$file
 done
+
+cd $CMSSW_BASE/
+cd ../
+for file in $(ls *condor)
+do
+    echo /store/user/${USER}/boostedZPlusMET/$output_dir/$file
+    xrdcp -f $file root://cmseos.fnal.gov//store/user/${USER}/boostedZPlusMET/$output_dir/$file
+done
+
+for file in $(ls *stderr)
+do
+    echo /store/user/${USER}/boostedZPlusMET/$output_dir/$file
+    xrdcp -f $file root://cmseos.fnal.gov//store/user/${USER}/boostedZPlusMET/$output_dir/$file
+done
+
+for file in $(ls *stdout)
+do
+    echo /store/user/${USER}/boostedZPlusMET/$output_dir/$file
+    xrdcp -f $file root://cmseos.fnal.gov//store/user/${USER}/boostedZPlusMET/$output_dir/$file
+done
+
