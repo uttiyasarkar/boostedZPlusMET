f21e3ca98aa010947580c92afd14365b9f7cdf5a
rpatelCERN <rpatel@cern.ch>
2018-01-29 04:01:03 -0600

    Bool for W Matches

--- src/skimMassSearchRegion.cc
+++ src/skimMassSearchRegion.cc
@@ -81,99 +81,6 @@ int main(int argc, char** argv){
     if( reg == skimSamples::kLowDphi )
         regionName="_lowDphi";
     outputFile = new TFile("SkimFileMass"+cutName+regionName+".root","RECREATE");
-    /*
-
-    typedef plot<RA2bTree> plot;
-    double mJbins[4]={50.,85.,135.,250.};
-    vector<vector<plot> > plots;
-    
-    for( int i = 0 ; i < numMETbins ; i++ ) {
-        TString tag="_";
-        tag+=lowestMET+i*binWidth;
-        vector<plot> plotsTemp;
-        plotsTemp.push_back(plot(*fillLeadingJetMass<RA2bTree>,"mJ_tagSR"+tag,"m_{J} [GeV]",3,mJbins));
-        plotsTemp.push_back(plot(*fillLeadingJetMass<RA2bTree>,"mJ_tagSB"+tag,"m_{J} [GeV]",3,mJbins));
-    
-        plotsTemp.push_back(plot(*fillLeadingJetMass<RA2bTree>,"mJ_antitagSR"+tag,"m_{J} [GeV]",3,mJbins));
-        plotsTemp.push_back(plot(*fillLeadingJetMass<RA2bTree>,"mJ_antitagSB"+tag,"m_{J} [GeV]",3,mJbins));
-    
-        plotsTemp.push_back(plot(*fillLeadingJetMass<RA2bTree>,"mJ_doubletagSR"+tag,"m_{J} [GeV]",3,mJbins));
-        plotsTemp.push_back(plot(*fillLeadingJetMass<RA2bTree>,"mJ_doubletagSB"+tag,"m_{J} [GeV]",3,mJbins));
-
-        plots.push_back(plotsTemp);
-    }
-
-    //vector<plot> tempPlots;
-    double met_bins[9]={100.,150.,200.,250.,300.,400.,500.,600.,700.};
-    plot METfine_Plot(*fillMET<RA2bTree>,"METfine","MET [GeV]",8,met_bins);
-    plot MET_Plot(*fillMET<RA2bTree>,"MET","MET [GeV]",3,100,700);
-    plot J1pt_Ptplot(*fillLeadingJetPt<RA2bTree>,"J1pt_Pt","p_{T,J} [GeV]",50,300.,1300.);
-    plot J2pt_Ptplot(*fillSubLeadingJetPt<RA2bTree>,"J2pt_Pt","p_{T,J} [GeV]",50,300.,1300.);
-    plot J1pt_Mplot(*fillLeadingJetMass<RA2bTree>,"J1pt_M","m_{J} [GeV]",50,50.,250.);
-    plot J2pt_Mplot(*fillSubLeadingJetMass<RA2bTree>,"J2pt_M","m_{J} [GeV]",50,50.,250.);
-    plot ClosestMass(*fillClosestJetMass<RA2bTree>,"ClosestMass","m_{J} [GeV]",3,mJbins);
-    plot FarthestMass(*fillFarthestJetMass<RA2bTree>,"FarthestMass","m_{J} [GeV]",3,mJbins);
-
-    vector<plot> doubletagSRPlots;
-    doubletagSRPlots.push_back(plot(MET_Plot));
-    doubletagSRPlots.push_back(plot(METfine_Plot));
-    doubletagSRPlots.push_back(plot(J1pt_Ptplot));
-    doubletagSRPlots.push_back(plot(J2pt_Ptplot));
-    doubletagSRPlots.push_back(plot(J1pt_Mplot));
-    doubletagSRPlots.push_back(plot(J2pt_Mplot));
-    doubletagSRPlots.push_back(plot(ClosestMass));
-    doubletagSRPlots.push_back(plot(FarthestMass));
-
-    vector<plot> doubletagSBPlots;
-    doubletagSBPlots.push_back(plot(MET_Plot));
-    doubletagSBPlots.push_back(plot(METfine_Plot));
-    doubletagSBPlots.push_back(plot(J1pt_Ptplot));
-    doubletagSBPlots.push_back(plot(J2pt_Ptplot));
-    doubletagSBPlots.push_back(plot(J1pt_Mplot));    
-    doubletagSBPlots.push_back(plot(J2pt_Mplot));    
-    doubletagSBPlots.push_back(plot(ClosestMass));
-    doubletagSBPlots.push_back(plot(FarthestMass));
-
-    vector<plot> tagSRPlots;
-    tagSRPlots.push_back(plot(MET_Plot));
-    tagSRPlots.push_back(plot(METfine_Plot));
-    tagSRPlots.push_back(plot(J1pt_Ptplot));
-    tagSRPlots.push_back(plot(J2pt_Ptplot));    
-    tagSRPlots.push_back(plot(J1pt_Mplot));
-    tagSRPlots.push_back(plot(J2pt_Mplot));    
-    tagSRPlots.push_back(plot(ClosestMass));
-    tagSRPlots.push_back(plot(FarthestMass));
-
-    vector<plot> tagSBPlots;
-    tagSBPlots.push_back(plot(MET_Plot));
-    tagSBPlots.push_back(plot(METfine_Plot));
-    tagSBPlots.push_back(plot(J1pt_Ptplot));
-    tagSBPlots.push_back(plot(J2pt_Ptplot));    
-    tagSBPlots.push_back(plot(J1pt_Mplot));
-    tagSBPlots.push_back(plot(J2pt_Mplot));    
-    tagSBPlots.push_back(plot(ClosestMass));
-    tagSBPlots.push_back(plot(FarthestMass));
-
-    vector<plot> antitagSRPlots;
-    antitagSRPlots.push_back(plot(MET_Plot));
-    antitagSRPlots.push_back(plot(METfine_Plot));
-    antitagSRPlots.push_back(plot(J1pt_Ptplot));
-    antitagSRPlots.push_back(plot(J2pt_Ptplot));    
-    antitagSRPlots.push_back(plot(J1pt_Mplot));
-    antitagSRPlots.push_back(plot(J2pt_Mplot));    
-    antitagSRPlots.push_back(plot(ClosestMass));
-    antitagSRPlots.push_back(plot(FarthestMass));
-
-    vector<plot> antitagSBPlots;
-    antitagSBPlots.push_back(plot(MET_Plot));
-    antitagSBPlots.push_back(plot(METfine_Plot));
-    antitagSBPlots.push_back(plot(J1pt_Ptplot));
-    antitagSBPlots.push_back(plot(J2pt_Ptplot));    
-    antitagSBPlots.push_back(plot(J1pt_Mplot));
-    antitagSBPlots.push_back(plot(J2pt_Mplot));    
-    antitagSBPlots.push_back(plot(ClosestMass));
-    antitagSBPlots.push_back(plot(FarthestMass));
-*/
     // background MC samples - 0 lepton regions
     for( int iSample = 0 ; iSample < skims.ntuples.size() ; iSample++){
 
@@ -185,7 +92,10 @@ int main(int argc, char** argv){
 	int BTags;	
         double MET,Weight,JetPt1, JetPt2,PrunedMass1, PrunedMass2, Jet1_tau2overtau1, Jet2_tau2overtau1;
         TBranch*b_BTags, *b_Weight,*b_MET,*b_JetPt1, *b_JetPt2,*b_PrunedMass1, *b_PrunedMass2, *b_Jet1_tau2overtau1, *b_Jet2_tau2overtau1;
+    	int WMatchedJet1, WMatchedJet2;
 
+        newtree->Branch("WMatchedJet1", &WMatchedJet1, "WMatchedJet1/I");	
+        newtree->Branch("WMatchedJet2", &WMatchedJet2, "WMatchedJet2/I");	
 	
         newtree->Branch("JetPt1", &JetPt1, "JetPt1/D");	
         newtree->Branch("JetPt2", &JetPt2, "JetPt/D");	
@@ -206,8 +116,8 @@ int main(int argc, char** argv){
         bool passBaseline;
         double jetMass1,jetMass2;
         TString filename;
-       for( int iEvt = 0 ; iEvt < min(options.MAX_EVENTS,numEvents) ; iEvt++ ){
-       //for( int iEvt = 0 ; iEvt <10; iEvt++ ){
+    for( int iEvt = 0 ; iEvt < min(options.MAX_EVENTS,numEvents) ; iEvt++ ){
+      // for( int iEvt = 0 ; iEvt <1000; iEvt++ ){
             ntuple->GetEntry(iEvt);
             if( iEvt % 100000 == 0 ) cout << skims.sampleName[iSample] << ": " << iEvt << "/" << min(options.MAX_EVENTS,numEvents) << endl;
              passBaseline=true;
@@ -241,6 +151,22 @@ int main(int argc, char** argv){
 	    Jet1_tau2overtau1=ntuple->JetsAK8_NsubjettinessTau2->at(0)/ntuple->JetsAK8_NsubjettinessTau1->at(0);
 	    Jet2_tau2overtau1=ntuple->JetsAK8_NsubjettinessTau2->at(1)/ntuple->JetsAK8_NsubjettinessTau1->at(1);
 	    //std::cout<<"MET"<<MET<<std::endl;
+	WMatchedJet1=0;
+	WMatchedJet2=0;
+	if(skims.sampleName[iSample]!="data"){
+	for( int i=0 ; i < ntuple->GenParticles->size() ; i++ ){
+        	if( abs(ntuple->GenParticles_PdgId->at(i)) == 24){ //&& ntuple->JetsAK8->at(0).DeltaR(ntuple->GenParticles->at(i))<0.4){
+			float deta=ntuple->JetsAK8->at(0).Eta()-ntuple->GenParticles->at(i).Eta();
+			float dphi=ntuple->JetsAK8->at(0).Phi()-ntuple->GenParticles->at(i).Phi();
+			if(sqrt((deta*deta)+(dphi*dphi))<0.4)WMatchedJet1=1;
+			else WMatchedJet1=0;
+			deta=ntuple->JetsAK8->at(1).Eta()-ntuple->GenParticles->at(i).Eta();
+			dphi=ntuple->JetsAK8->at(1).Phi()-ntuple->GenParticles->at(i).Phi();
+			if(sqrt((deta*deta)+(dphi*dphi))<0.4)WMatchedJet2=1;
+			else WMatchedJet2=0;
+		}
+    	     }
+	}
 	    newtree->Fill(); 
         }// end event loop
 	//newtree->SetName(filename);
@@ -249,7 +175,7 @@ int main(int argc, char** argv){
 	  
   }// end sample loop
   
-
+/*
     for( int iSample = 0 ; iSample < skims.signalNtuples.size() ; iSample++){
         RA2bTree* ntuple = skims.signalNtuples[iSample];
 
@@ -261,7 +187,7 @@ int main(int argc, char** argv){
 	int BTags;	
         double MET,Weight,JetPt1, JetPt2,PrunedMass1, PrunedMass2, Jet1_tau2overtau1, Jet2_tau2overtau1;
         TBranch*b_BTags, *b_Weight,*b_MET,*b_JetPt1, *b_JetPt2,*b_PrunedMass1, *b_PrunedMass2, *b_Jet1_tau2overtau1, *b_Jet2_tau2overtau1;
-	
+		
         newtree->Branch("JetPt1", &JetPt1, "JetPt1/D");	
         newtree->Branch("JetPt2", &JetPt2, "JetPt/D");	
         newtree->Branch("PrunedMass1", &PrunedMass1, "PrunedMass1/D");	
@@ -303,33 +229,8 @@ int main(int argc, char** argv){
 	//newtree->SetName(filename);
 	outputFile->cd();
 	newtree->Write(skims.signalSampleName[iSample]);
-   } 
-
- /* 
-    for( int iBin = 0 ; iBin < numMETbins ; iBin++){
-        for( int iPlot = 0 ; iPlot < plots[iBin].size() ; iPlot++){
-            plots[iBin][iPlot].addDataNtuple(ntuple,"data");
-        }
-    }
-    for( int i = 0 ; i < doubletagSRPlots.size() ; i++ ){
-        doubletagSRPlots[i].addDataNtuple(ntuple,"doubletagSR_data");
-    }
-    for( int i = 0 ; i < doubletagSBPlots.size() ; i++ ){
-        doubletagSBPlots[i].addDataNtuple(ntuple,"doubletagSB_data");
-    }
-    for( int i = 0 ; i < tagSRPlots.size() ; i++ ){
-        tagSRPlots[i].addDataNtuple(ntuple,"tagSR_data");
-    }
-    for( int i = 0 ; i < tagSBPlots.size() ; i++ ){
-        tagSBPlots[i].addDataNtuple(ntuple,"tagSB_data");
-    }
-    for( int i = 0 ; i < antitagSRPlots.size() ; i++ ){
-        antitagSRPlots[i].addDataNtuple(ntuple,"antitagSR_data");
-    }
-    for( int i = 0 ; i < antitagSBPlots.size() ; i++ ){
-        antitagSBPlots[i].addDataNtuple(ntuple,"antitagSB_data");
-    }
-*/
+   }
+*/ 
     // data 
 /*
     RA2bTree* ntuple = skims.dataNtuple;
